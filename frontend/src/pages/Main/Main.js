c11936ce44dc864be62710a79cce99d23ab406f2
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2020-01-29 16:10:19 -0300

    edit main page and add backend request

--- frontend/src/pages/Main/Main.js
+++ frontend/src/pages/Main/Main.js
@@ -1,100 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Main.css";
 
+import api from "../../services/api";
 import logo from "../../assets/logo.png";
+import icon from "../../assets/icon.png";
 import like from "../../assets/like.svg";
 import dislike from "../../assets/dislike.svg";
 
 export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs", {
+        headers: {
+          user: match.params.id
+        }
+      });
+      setUsers(response.data);
+    }
+    loadUsers();
+  }, [match.params.id]);
+
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/Likes`, null, {
+      headers: { user: match.params.id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: { user: match.params.id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
   return (
     <div className="main-container">
-      <img src={logo} className="logo" alt="Tinder" />
-      <ul>
-        <li>
-          <img
-            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
-            alt="photo"
-          />
-          <footer>
-            <strong>Diego Schell</strong>
-            <p>
-              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
-              desenvolvimento web e
-              mobile.dassssssssssssssssssssssssssssssssssssssssssasdasdasdasdsadasdasdasdasdasdasdasdasdasdsssssssssssssss
-            </p>
-          </footer>
+      <Link to="/">
+        <img src={logo} className="logo" alt="Tinder" />
+      </Link>
 
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="like" />
-            </button>
-          </div>
-        </li>
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt={user.name} />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
 
-        <li>
-          <img
-            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
-            alt="photo"
-          />
-          <footer>
-            <strong>Diego Schell</strong>
-            <p>
-              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
-              desenvolvimento web e mobile.
-            </p>
-          </footer>
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="Dislike" />
+                </button>
 
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-              <img src={like} alt="like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
-            alt="photo"
-          />
-          <footer>
-            <strong>Diego Schell</strong>
-            <p>
-              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
-              desenvolvimento web e mobile.
-            </p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-              <img src={like} alt="like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
-            alt="photo"
-          />
-          <footer>
-            <strong>Diego Schell</strong>
-            <p>
-              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
-              desenvolvimento web e mobile.
-            </p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike" />
-              <img src={like} alt="like" />
-            </button>
-          </div>
-        </li>
-      </ul>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={like} alt="Like" />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">
+          <img src={icon} alt="Tinder" className="icon" />
+          <h2>There's no one else here.</h2>
+        </div>
+      )}
     </div>
   );
 }
