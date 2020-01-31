6d8c79e6b447bc03c9786d86ba4977a4346fab9a
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2020-01-31 16:52:20 -0300

    add socket.io and edit http server

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -1,26 +1,34 @@
-const Dev = require('../models/Dev')
+const Dev = require("../models/Dev");
 
-module.exports ={
-    async store(req,res){
+module.exports = {
+  async store(req, res) {
+    const { user } = req.headers;
+    const { devId } = req.params;
 
-        const { user } = req.headers
-        const { devId } = req.params
-        
-        const loggedDev = await Dev.findById(user)
-        const targetDev = await Dev.findById(devId)
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devId);
 
-        if(!targetDev){
-            return res.status(400).send({error:'Dev not exists'})
-        }
-
-        if(targetDev.likes.includes(loggedDev._id)){
-            console.log('DEU MATCH')
-        }
-
-        loggedDev.likes.push(targetDev._id)
-
-        await loggedDev.save()
-
-        return res.json(loggedDev)
+    if (!targetDev) {
+      return res.status(400).send({ error: "Dev not exists" });
     }
-}
+
+    if (targetDev.likes.includes(loggedDev._id)) {
+      const loggedSocket = req.connectedUsers[user];
+      const targetSocket = req.connectedUsers[devId];
+
+      if (loggedSocket) {
+        req.io.to(loggedSocket).emit("match", targetDev);
+      }
+
+      if (targetSocket) {
+        req.io.to(targetSocket).emit("match", loggedDev);
+      }
+    }
+
+    loggedDev.likes.push(targetDev._id);
+
+    await loggedDev.save();
+
+    return res.json(loggedDev);
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const routes = require('./routes')
-const mongoose = require('./database')
-const cors = require('cors')
-const server = express()
+const express = require("express");
+const routes = require("./routes");
+const mongoose = require("./database");
+const cors = require("cors");
 
-server.use(cors())
-server.use(express.json())
-server.use(routes)
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-server.listen(3333, () => console.log('Running on 3333'))
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user } = socket.handshake.query;
+
+  connectedUsers[user] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333, () => console.log("Running on 3333"));
